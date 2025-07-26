3ce2917ff365f9f200e864e11482d88ce9a61d8e
sirivellaramudu <40568559+sirivellaramudu@users.noreply.github.com>
2025-07-26 21:53:30 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,5 @@
 import { io } from 'socket.io-client';
 
 const URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:4000';
-export const socket = io(URL, { autoConnect: false });
+export const socket = io(URL, { autoConnect: false,
+                               withCredentials: true });
